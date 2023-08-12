b4c6fcef75fa448eb697403b4ca1aedc2784d3f1
Ihor Fedorchenko <ihor.fedorchenko@evowill.com>
2023-08-12 14:39:28 +0300

    project skeleton

---
+++ src/main/java/com/ua/kpi/DevelopmentAutomation/DevelopmentAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.ua.kpi.DevelopmentAutomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopmentAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ua/kpi/DevelopmentAutomation/DevelopmentAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ua.kpi.DevelopmentAutomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopmentAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
